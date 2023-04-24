be4ed159b6ddfd4822dc8c71a1714eb3b27adf1f
Your Name <you@example.com>
2023-04-24 16:46:26 +0200

    new

---
+++ src/main/java/com/example/coche/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.coche;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+    @FXML
+    private Button cliente;
+    @FXML
+    private Button admin;
+    @FXML
+    private Button registrar;
+}
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+    }
+}
